85bc1e8214ac0de2baac3886d71f589d7033c083
Alberto Camrgo <amcamargo95@gmail.com>
2016-05-16 11:50:05 -0500

    minor changes

---
+++ app/src/main/java/angelcotes/fragments/HelpOne.java
@@ -0,0 +1,27 @@
+package angelcotes.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import angelcotes.viewmovil.R;
+
+/**
+ * Created by Alberto Mario Camargo Castro on 16-May-16.
+ */
+public class HelpOne  extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.activity_fullscreen, container, false);
+        return rootView;
+
+    }
+
+
+}
+
+
